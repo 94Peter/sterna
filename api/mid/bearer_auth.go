e96ae0b55fc8758d958745ac951253566ad0759e
Peter Chen <ch.focke@gmail.com>
2022-04-16 15:41:59 +0800

    feat: add bearer auth

---
+++ api/mid/bearer_auth.go
@@ -0,0 +1,124 @@
+package mid
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/94peter/sterna/auth"
+	"github.com/94peter/sterna/log"
+	"github.com/94peter/sterna/util"
+	"github.com/gorilla/mux"
+)
+
+type TokenParserResult interface {
+	Host() string
+	Perms() []string
+}
+
+type AuthTokenParser interface {
+	Parser(token string) (TokenParserResult, error)
+}
+
+func NewBearerAuthMid(tokenParser AuthTokenParser) AuthMidInter {
+	return &bearAuthMiddle{
+		parser:   tokenParser.Parser,
+		authMap:  make(map[string]uint8),
+		groupMap: make(map[string][]auth.UserPerm),
+	}
+}
+
+func (lm *bearAuthMiddle) GetName() string {
+	return "auth"
+}
+
+type bearAuthMiddle struct {
+	parser   func(token string) (TokenParserResult, error)
+	log      log.Logger
+	authMap  map[string]uint8
+	groupMap map[string][]auth.UserPerm
+}
+
+const (
+	BearerAuthTokenKey = "Authorization"
+)
+
+func (am *bearAuthMiddle) AddAuthPath(path string, method string, isAuth bool, group []auth.UserPerm) {
+	value := uint8(0)
+	if isAuth {
+		value = value | authValue
+	}
+	key := getPathKey(path, method)
+	am.authMap[key] = uint8(value)
+	am.groupMap[key] = group
+}
+
+func (am *bearAuthMiddle) IsAuth(path string, method string) bool {
+	key := getPathKey(path, method)
+	value, ok := am.authMap[key]
+	if ok {
+		return (value & authValue) > 0
+	}
+	return false
+}
+
+func (am *bearAuthMiddle) HasPerm(path, method string, perm []string) bool {
+	key := fmt.Sprintf("%s:%s", path, method)
+	groupAry, ok := am.groupMap[key]
+	if !ok || groupAry == nil || len(groupAry) == 0 {
+		return true
+	}
+	for _, g := range groupAry {
+		if util.IsStrInList(string(g), perm...) {
+			return true
+		}
+	}
+	return false
+}
+
+func (am *bearAuthMiddle) GetMiddleWare() func(f http.HandlerFunc) http.HandlerFunc {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		// one time scope setup area for middleware
+		return func(w http.ResponseWriter, r *http.Request) {
+			path, err := mux.CurrentRoute(r).GetPathTemplate()
+			if err != nil {
+				w.WriteHeader(http.StatusNotFound)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			if am.IsAuth(path, r.Method) {
+				authToken := r.Header.Get(AuthTokenKey)
+				if authToken == "" {
+					w.WriteHeader(http.StatusUnauthorized)
+					w.Write([]byte("miss token"))
+					return
+				}
+				if !strings.HasPrefix(authToken, "Bearer ") {
+					w.WriteHeader(http.StatusUnauthorized)
+					w.Write([]byte("invalid token: missing Bearer"))
+					return
+				}
+				authToken = authToken[7:]
+				result, err := am.parser(authToken)
+				if err != nil {
+					w.WriteHeader(http.StatusUnauthorized)
+					w.Write([]byte("invalid token: " + err.Error()))
+					return
+				}
+				if result.Host() != r.Host {
+					w.WriteHeader(http.StatusUnauthorized)
+					w.Write([]byte(fmt.Sprintf("host not match: [%s] is not [%s]", result.Host(), r.Host)))
+					return
+				}
+
+				if hasPerm := am.HasPerm(path, r.Method, result.Perms()); !hasPerm {
+					w.WriteHeader(http.StatusUnauthorized)
+					w.Write([]byte("permission error"))
+					return
+				}
+				r = util.SetCtxKeyVal(r, auth.CtxUserInfoKey, result)
+			}
+			f(w, r)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/NaySoftware/go-fcm v0.0.0-20190516140123-808e978ddcd2
 	github.com/RichardKnop/machinery v1.10.6
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/blend/go-sdk v1.20220212.5
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/eclipse/paho.mqtt.golang v1.3.5
 	github.com/go-redis/redis/v8 v8.11.4
@@ -20,9 +21,9 @@ require (
 	github.com/signintech/gopdf v0.9.21
 	github.com/stretchr/testify v1.7.0
 	go.mongodb.org/mongo-driver v1.7.2
-	golang.org/x/net v0.0.0-20210903162142-ad29c8ab022f
-	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f
-	golang.org/x/text v0.3.6
-	google.golang.org/api v0.56.0
+	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
+	golang.org/x/text v0.3.7
+	google.golang.org/api v0.62.0
 	gopkg.in/yaml.v2 v2.4.0
 )
